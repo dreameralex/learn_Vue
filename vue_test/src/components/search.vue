a830771afdfbff7f0a7640118c5cbde0b7b9eff9
Alex's PC <zhangyh8216@sina.com>
2024-06-16 20:15:15 +0800

    To 99

---
+++ vue_test/src/components/search.vue
@@ -0,0 +1,33 @@
+<template>
+  	<section class="jumbotron">
+	<h3 class="jumbotron-heading">Search Github Users</h3>
+	<div>
+		<input type="text" placeholder="enter the name you search" v-model="keyWord"/>&nbsp;
+		<button @click="searchUsers">Search</button>
+	</div>
+	</section>
+</template>
+<script>
+	import axios from 'axios'
+	export default {
+		name: 'Search',
+		data() {
+			return{
+				keyWord:''
+			}
+		},
+		methods:{
+			searchUsers(){
+				axios.get(`https://api.github.com/search/users?q=${this.keyWord}`).then(
+					Response => {
+						// console.log('Success!',Response.data)
+						this.$bus.$emit('getUsers',Response.data.items)
+					},
+					error => {
+						console.log('Failed!',error.message)
+					}
+				)
+			}
+		}
+	}
+</script>
